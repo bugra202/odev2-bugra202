dc541b4cd50e19012c489683d6369005f4c7a2c2
bugratasdemir <bugraa202@gmail.com>
2024-02-23 23:18:31 +0300

    Update general package in all class.

--- odev2-bugra202/src/main/java/com/bugratasdemir/odev2bugra202/request/ProductSaveRequest.java
+++ odev2-bugra202/src/main/java/com/bugratasdemir/odev2bugra202/request/ProductSaveRequest.java
@@ -1,7 +1,6 @@
 package com.bugratasdemir.odev2bugra202.request;
 
 import com.bugratasdemir.odev2bugra202.entity.Category;
-import com.bugratasdemir.odev2bugra202.enums.ProductStatus;
 
 import java.time.LocalDateTime;
 
@@ -9,5 +8,4 @@ public record ProductSaveRequest(String name,
                                  Double price,
                                  LocalDateTime expirationDate,
                                  String description,
-                                 ProductStatus status,
                                  Category category) {}
